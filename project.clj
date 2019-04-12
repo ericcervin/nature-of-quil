896bf9bd1fcdf933abd0e0f9b974b899b8cfdff4
Eric Ervin <ericcervin@gmail.com>
2019-04-11 22:06:39 -0400

    added chap_01_ex_05.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject nature-of-quil "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [quil "2.6.0" :exclusions [org.clojure/clojure]]
-                 [org.clojure/data.json "0.2.6"]])
+                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/math.numeric-tower "0.0.4"]])
--- src/nature_of_quil/chap_01_ex_05.clj
+++ src/nature_of_quil/chap_01_ex_05.clj
@@ -1,21 +1,34 @@
-(ns nature-of-quil.chap_01_ex_04
-  (:require [quil.core :as q]))
+(ns nature-of-quil.chap_01_ex_05
+  (:require [quil.core :as q]
+            [clojure.math.numeric-tower :as math]))
 
 
 
 
 (defn setup [])  
      
+(defn magnitude [x1 y1 x2 y2]
+  (let [dx (- x2 x1)
+        dy (- y2 y1)]
+    (math/sqrt (+ (* dx dx) (* dy dy)))))
+    
 
 (defn draw []
   (let [mouse [(q/mouse-x) (q/mouse-y)]
         center [(/ (q/width) 2) (/ (q/height) 2)]
         mouse (map #(- %1 %2) mouse center)
-        mouse (map #(* % 0.5) mouse)]
+        mag (magnitude 0 0 (first mouse) (second mouse))]
+        
     
     (q/background 255)
+    
+    (q/fill 0)
+    (q/rect 0 0 mag 10)
+    
     (q/translate (first center) (second center))
     (q/line 0 0 (first mouse) (second mouse))))
+    
+    
     
       
 (q/defsketch practice
